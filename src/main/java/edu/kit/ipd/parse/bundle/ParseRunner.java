f44e9c222731c673980128a9d756ca87ae9c9814
Sebastian Weigelt <weigelt@kit.edu>
2021-01-11 12:36:44 +0100

    removed otion to print agg graph

--- src/main/java/edu/kit/ipd/parse/bundle/ParseRunner.java
+++ src/main/java/edu/kit/ipd/parse/bundle/ParseRunner.java
@@ -20,8 +20,6 @@ import org.apache.commons.io.FileUtils;
 
 import edu.kit.ipd.parse.luna.Luna;
 import edu.kit.ipd.parse.luna.LunaRunException;
-import edu.kit.ipd.parse.luna.graph.AGGGraphCreator;
-import edu.kit.ipd.parse.luna.graph.ParseGraph;
 import edu.kit.ipd.parse.luna.tools.ConfigManager;
 import edu.kit.ipd.parse.luna.tools.StringToHypothesis;
 import edu.kit.ipd.parse.voice_recorder.VoiceRecorder;
@@ -117,8 +115,9 @@ public class ParseRunner {
 		}
 
 		if (cmd.hasOption(CMD_OPTION_SAVE_TO_FILE)) {
-			AGGGraphCreator agggc = new AGGGraphCreator((ParseGraph) luna.getMainGraph());
-			agggc.saveTo(cmd.getOptionValue(CMD_OPTION_SAVE_TO_FILE), Paths.get(".").toAbsolutePath().normalize().toString());
+			//TODO: implement me!
+			//			AGGGraphCreator agggc = new AGGGraphCreator((ParseGraph) luna.getMainGraph());
+			//			agggc.saveTo(cmd.getOptionValue(CMD_OPTION_SAVE_TO_FILE), Paths.get(".").toAbsolutePath().normalize().toString());
 		}
 
 		System.exit(0);
